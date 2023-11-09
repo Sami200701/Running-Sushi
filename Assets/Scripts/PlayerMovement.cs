716af9de9ac88d7f8749d7c82ee1c06f24997491
Alexander Zerpa <alexzerpawanner@gmail.com>
2023-11-09 17:53:57 +0100

    Cambiado sistema de movimiento a usar AddForce
    Creado método Run()
    Parametrización de aceleración y posibilidad de cambiarla en el aire
    Pequeña limpieza de código
    Lógica de salto pasada a método Jump()

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
+    public float accel;
+    public float decel;
+
+    public float inAirAccel;
+
     public float jumpHeight;
 
     public float gravityScale;
@@ -17,7 +22,9 @@ public class PlayerMovement : MonoBehaviour
 
     public LayerMask jumpableGround;
 
-    public bool facingRight;
+    public bool conserveMomentum;
+
+    private bool facingRight;
 
     private bool jumping;
     private bool jumpCancelled;
@@ -25,7 +32,7 @@ public class PlayerMovement : MonoBehaviour
     private float coyoteTimeCounter;
     private float jumpBufferCounter;
 
-    private float x;
+    private float moveInput;
     private Rigidbody2D rb;
     private BoxCollider2D coll;
 
@@ -33,45 +40,39 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.gravityScale = gravityScale;
+        rb.gravityScale = fallGravityScale;
 
         coll = GetComponent<BoxCollider2D>();
+
+        facingRight = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        x = Input.GetAxis("Horizontal");
+        moveInput = Input.GetAxis("Horizontal");
 
-        Flip();
+        coyoteTimeCounter -= Time.deltaTime;
+        jumpBufferCounter -= Time.deltaTime;
+
+        if (moveInput != 0)
+        {
+            Flip();
+        }
 
         if (IsGrounded())
         {
             coyoteTimeCounter = coyoteTime;
-        }
-        else
-        {
-            coyoteTimeCounter -= Time.deltaTime;
         }
 
         if (Input.GetButtonDown("Jump") && !jumping)
         {
             jumpBufferCounter = jumpBuffer;
         }
-        else
-        {
-            jumpBufferCounter -= Time.deltaTime;
-        }
 
         if (jumpBufferCounter > 0f && coyoteTimeCounter > 0f)
         {
-            rb.gravityScale = gravityScale;
-            float jumpForce = Mathf.Sqrt(jumpHeight * (Physics2D.gravity.y * rb.gravityScale) * -2f) * rb.mass;
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            jumping = true;
-            jumpCancelled = false;
-            jumpBufferCounter = 0f;
-            coyoteTimeCounter = 0f;
+            Jump();
         }
 
         if (jumping)
@@ -93,7 +94,8 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(x * speed, rb.velocity.y);
+        // rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        Run();
 
         if (jumpCancelled && jumping && rb.velocity.y > 0f)
         {
@@ -106,9 +108,46 @@ public class PlayerMovement : MonoBehaviour
         return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, 0.1f, jumpableGround);
     }
 
+    private void Run()
+    {
+        float tspeed = moveInput * speed;
+        float speedDiff = tspeed - rb.velocity.x;
+        float accelRate = (Mathf.Abs(tspeed) > 0.01f) ? accel : decel;
+        if (!IsGrounded())
+        {
+            accelRate *= inAirAccel;
+        }
+
+        if (conserveMomentum && Mathf.Abs(rb.velocity.x) > Mathf.Abs(tspeed) && rb.velocity.x * tspeed > 0 && !IsGrounded())
+        {
+            accelRate = 0;
+        }
+
+        float movement = speedDiff * accelRate;
+
+        rb.AddForce(movement * Vector2.right, ForceMode2D.Force);
+    }
+
+    private void Jump()
+    {
+        rb.gravityScale = gravityScale;
+        float jumpForce = Mathf.Sqrt(jumpHeight * (Physics2D.gravity.y * rb.gravityScale) * -2f) * rb.mass;
+
+        if (rb.velocity.y < 0)
+        {
+            jumpForce -= rb.velocity.y;
+        }
+
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        jumping = true;
+        jumpCancelled = false;
+        jumpBufferCounter = 0f;
+        coyoteTimeCounter = 0f;
+    }
+
     private void Flip()
     {
-        if (facingRight && x < 0f || !facingRight && x > 0f)
+        if (facingRight && moveInput < 0f || !facingRight && moveInput > 0f)
         {
             facingRight = !facingRight;
             Vector3 localScale = transform.localScale;
